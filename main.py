27656a65ed4a0143e569abaec6d74d5b0ac9ee4f
Samrood Ali <samroodali@live.com>
2021-01-20 11:48:08 +0000

    Fix:changed title function to capitalize

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ from LOGO import LOGO
 print(LOGO)
 print("Hello and welcome to Flight Club new user registration page\n")
 
+# Collecting new user's first name , last name and email address
+first_name = input("Please Enter your first Name\n").capitalize()
+last_name = input("Please Enter your last Name\n").capitalize()
+email1 = input("Please enter your email address\n")
+email2 =  input("Please comfirm your email address\n")
