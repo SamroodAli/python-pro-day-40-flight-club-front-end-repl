2dc3fc2c497a4f7bd71983c99a645cdca5735d74
Samrood Ali <samroodali@live.com>
2021-01-20 11:43:10 +0000

    API:Update google sheets using Sheety API function

---
+++ sheety_api.py
@@ -0,0 +1,20 @@
+import os
+import requests
+
+# Sheety API authentication and endpoint
+username = os.getenv("USERNAME")
+password = os.getenv("PASSWORD")
+endpoint = os.getenv("ENDPOINT")
+
+# Update Google Sheets Data using Sheety API
+def update_sheets(first_name,last_name,email):
+    new_user = {
+        "user": {
+        "first":first_name,
+        "last":last_name,
+        "email":email
+        }
+    }
+    response = requests.post(url=endpoint, auth=(username,password), json=new_user)
+    response.raise_for_status()
+    
